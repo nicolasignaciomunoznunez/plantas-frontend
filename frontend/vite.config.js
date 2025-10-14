1ebef70e5be9b4801a0293071a59f3066dab9db0
Nicolás Muñoz <munoznuneznicolas@gmail.com>
2025-10-14 13:40:53 -0300

    cambios en el config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
